c2f833fb05d5525a444d04e73160f20b5816acf6
Shenel Kurera <wsakurera@gmail.com>
2024-05-18 11:10:45 +0200

    applied api data to sample table component

--- FrontendDashoboardVuestic/src/pages/books/BooksPage.vue
+++ FrontendDashoboardVuestic/src/pages/books/BooksPage.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    <h1 class="page-title">Books</h1>
+  <h1 class="page-title">Books</h1>
 
-    <VaCard>
+  <VaCard>
+    <VaCardContent>
+      <div class="flex flex-col md:flex-row gap-2 mb-2 justify-between">
+        <div class="flex flex-col md:flex-row gap-2 justify-start">
+          <VaButtonToggle
+            v-model="doShowAsCards"
+            color="background-element"
+            border-color="background-element"
+            :options="[
+              { label: 'Cards', value: true },
+              { label: 'Table', value: false },
+            ]"
+          />
+        </div>
+        <VaButton icon="add" @click="createNewBook">Book</VaButton>
+      </div>
+
       <VaCardContent>
         <div>
-          <h1>Book List</h1>
+          <h1>yolo</h1>
+          <h2>{{ allBooks }}</h2>
           <ul>
             <li v-for="book in allBooks" :key="book.id">
               {{ book.name }}
@@ -13,13 +29,144 @@
           </ul>
         </div>
       </VaCardContent>
-    </VaCard>
-  </div>
-</template>
+      <ApiBookTable
+        :books="allBooks"
+        :loading="isLoading">
 
+      </ApiBookTable>
+      <BookCards
+        v-if="doShowAsCards"
+        :books="books"
+        :loading="isLoading"
+        @edit="editBook"
+        @delete="onBookDeleted"
+      />
+      <BookTable
+        v-else
+        v-model:sort-by="sorting.sortBy"
+        v-model:sorting-order="sorting.sortingOrder"
+        v-model:pagination="pagination"
+        :books="books"
+        :loading="isLoading"
+        @edit="editBook"
+        @delete="onBookDeleted"
+      />
+    </VaCardContent>
+
+    <VaModal
+      v-slot="{ cancel, ok }"
+      v-model="doShowBookFormModal"
+      size="small"
+      mobile-fullscreen
+      close-button
+      stateful
+      hide-default-actions
+      :before-cancel="beforeEditFormModalClose"
+    >
+      <h1 v-if="bookToEdit === null" class="va-h5 mb-4">Add book</h1>
+      <h1 v-else class="va-h5 mb-4">Edit book</h1>
+      <EditBookForm
+        ref="editFormRef"
+        :book="bookToEdit"
+        :save-button-label="bookToEdit === null ? 'Add' : 'Save'"
+        @close="cancel"
+        @save="
+          (book) => {
+            onBookSaved(book)
+            ok()
+          }
+        "
+      />
+    </VaModal>
+  </VaCard>
+</template>
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { ref , onMounted } from 'vue'
+import { useLocalStorage } from '@vueuse/core'
+import { useBooks } from './composables/useBooks'
+import BookCards from './widgets/BookCards.vue'
+import BookTable from './widgets/BooksTable.vue'
+import ApiBookTable from './widgets/ApiBooksTable.vue'
+
+import EditBookForm from './widgets/EditBookForm.vue'
+import { Book } from './types'
+import { useModal, useToast } from 'vuestic-ui'
+import axios from "axios" 
+
+const doShowAsCards = useLocalStorage('books-view', true)
+
+const { books, update, add, isLoading, remove, pagination, sorting } = useBooks()
+
+const bookToEdit = ref<Book | null>(null)
+const doShowBookFormModal = ref(false)
+
+const editBook = (book: Book) => {
+  bookToEdit.value = book
+  doShowBookFormModal.value = true
+}
+
+const createNewBook = () => {
+  bookToEdit.value = null
+  doShowBookFormModal.value = true
+}
+
+const { init: notify } = useToast()
+
+const onBookSaved = async (book: Book) => {
+  doShowBookFormModal.value = false
+  if ('id' in book) {
+    await update(book as Book)
+    notify({
+      message: 'Book updated',
+      color: 'success',
+    })
+  } else {
+    await add(book as Book)
+    notify({
+      message: 'Book created',
+      color: 'success',
+    })
+  }
+}
+
+const { confirm } = useModal()
+
+const onBookDeleted = async (book: Book) => {
+  const response = await confirm({
+    title: 'Delete book',
+    message: `Are you sure you want to delete book "${book.book_name}"?`,
+    okText: 'Delete',
+    size: 'small',
+    maxWidth: '380px',
+  })
+
+  if (!response) {
+    return
+  }
+
+  await remove(book)
+  notify({
+    message: 'Book deleted',
+    color: 'success',
+  })
+}
+
+const editFormRef = ref()
+
+const beforeEditFormModalClose = async (hide: () => unknown) => {
+  if (editFormRef.value.isFormHasUnsavedChanges) {
+    const agreed = await confirm({
+      maxWidth: '380px',
+      message: 'Form has unsaved changes. Are you sure you want to close it?',
+      size: 'small',
+    })
+    if (agreed) {
+      hide()
+    }
+  } else {
+    hide()
+  }
+}
 
 const allBooks = ref([]);
 const getAllBooks = async () => {
@@ -33,4 +180,5 @@ const getAllBooks = async () => {
 };
 
 onMounted(getAllBooks);
+
 </script>
---
+++ FrontendDashoboardVuestic/src/pages/books/widgets/ApiBooksTable.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+import { PropType, computed } from 'vue'
+import { defineVaDataTableColumns } from 'vuestic-ui'
+import { Book } from '../types'
+import UserAvatar from '../../users/widgets/UserAvatar.vue'
+import BookStatusBadge from '../components/BookStatusBadge.vue'
+import { Pagination, Sorting } from '../../../data/pages/books'
+import { useVModel } from '@vueuse/core'
+
+const columns = defineVaDataTableColumns([
+  { label: 'Book name', key: 'name', sortable: true },
+  { label: 'Book owner', key: 'book_owner', sortable: true },
+  { label: 'Category', key: 'categories', sortable: true },
+  { label: 'Status', key: 'status', sortable: true },
+  { label: 'Create By', key: 'created_by', sortable: true },
+  { label: 'Creation Date', key: 'created_at', sortable: true },
+  { label: ' ', key: 'actions' },
+])
+
+const props = defineProps({
+  books: {
+    type: Array as PropType<Book[]>,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    required: true,
+  },
+  sortBy: {
+    type: Object as PropType<Sorting['sortBy']>,
+    required: true,
+  },
+  sortingOrder: {
+    type: Object as PropType<Sorting['sortingOrder']>,
+    required: true,
+  },
+  pagination: {
+    type: Object as PropType<Pagination>,
+    required: true,
+  },
+})
+
+const emit = defineEmits<{
+  (event: 'edit', book: Book): void
+  (event: 'delete', book: Book): void
+}>()
+
+const avatarColor = (userName: string) => {
+  const colors = ['primary', '#FFD43A', '#ADFF00', '#262824', 'danger']
+  const index = userName.charCodeAt(0) % colors.length
+  return colors[index]
+}
+
+const sortByVModel = useVModel(props, 'sortBy', emit)
+const sortingOrderVModel = useVModel(props, 'sortingOrder', emit)
+
+const totalPages = computed(() => Math.ceil(props.pagination.total / props.pagination.perPage))
+</script>
+
+<template>
+  <div>
+    <VaDataTable
+      :items="books"
+      :columns="columns"
+      :loading="loading"
+    >
+      <template #cell(book_name)="{ rowData }">
+        <div class="ellipsis max-w-[230px] lg:max-w-[450px]">
+          {{ rowData.name }}
+        </div>
+      </template>
+      <template #cell(book_owner)="{ rowData }">
+        <div class="flex items-center gap-2 ellipsis max-w-[230px]">
+          {{ rowData.book_owner }}
+        </div>
+      </template>
+      <template #cell(categories)="{ rowData }">
+        <div class="flex items-center gap-2 ellipsis max-w-[230px]">
+          {{ rowData.categories }}
+        </div>
+        <!--<VaAvatarGroup
+          size="small"
+          :options="
+            (book as Book).team.map((user) => ({
+              label: user.fullname,
+              src: user.avatar,
+              fallbackText: user.fullname[0],
+              color: avatarColor(user.fullname),
+            }))
+          "
+          :max="5"
+        />-->
+      </template>
+      <!-- <template #cell(status)="{ rowData: book }">
+        <BookStatusBadge :status="book.status" />
+      </template> -->
+
+      <!-- <template #cell(actions)="{ rowData: book }">
+        <div class="flex gap-2 justify-end">
+          <VaButton
+            preset="primary"
+            size="small"
+            color="primary"
+            icon="mso-edit"
+            aria-label="Edit book"
+            @click="$emit('edit', book as Book)"
+          />
+          <VaButton
+            preset="primary"
+            size="small"
+            icon="mso-delete"
+            color="danger"
+            aria-label="Delete book"
+            @click="$emit('delete', book as Book)"
+          />
+        </div>
+      </template> -->
+    </VaDataTable>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.va-data-table {
+  ::v-deep(tbody .va-data-table__table-tr) {
+    border-bottom: 1px solid var(--va-background-border);
+  }
+}
+</style>
