e293c975af4884484b291c2e479e1cc82e3f53de
Shenel Kurera <wsakurera@gmail.com>
2024-09-24 20:52:06 +0200

    Fixed : Access to XMLHttpRequest at 'http://localhost:8000/sanctum/csrf-cookie' from origin 'http://localhost:3000' has been blocked by CORS policy: The 'Access-Control-Allow-Origin' header contains multiple values '*, http://localhost:3000', but only one is allowed.

--- FrontendDashoboardVuestic/src/axios.js
+++ FrontendDashoboardVuestic/src/axios.js
@@ -1,4 +1,9 @@
 import axios from "axios";
+window.axios = axios;
 
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8000";
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.baseURL = "http://localhost:8000";
+
+/*axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:8000";*/
