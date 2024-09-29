ce265697074066c50e77a841166e13b4c1a3e828
Shenel Kurera <wsakurera@gmail.com>
2024-09-29 18:34:38 +0200

    Code review

--- FrontendDashoboardVuestic/src/pages/books/BooksPage.vue
+++ FrontendDashoboardVuestic/src/pages/books/BooksPage.vue
@@ -34,34 +34,8 @@
           >
         </ApiBookTable>
       </section>
-    
     </VaCardContent>
 
-    <VaModal
-      v-slot="{ cancel, ok }"
-      v-model="doShowBookFormModal"
-      size="small"
-      mobile-fullscreen
-      close-button
-      stateful
-      hide-default-actions
-      :before-cancel="beforeEditFormModalClose"
-    >
-      <h1 v-if="bookToEdit === null" class="va-h5 mb-4">Add book</h1>
-      <h1 v-else class="va-h5 mb-4">Edit book</h1>
-      <EditBookForm
-        ref="editFormRef"
-        :book="bookToEdit"
-        :save-button-label="bookToEdit === null ? 'Add' : 'Save'"
-        @close="cancel"
-        @save="
-          (book) => {
-            onBookSaved(book)
-            ok()
-          }
-        "
-      />
-    </VaModal>
     <VaModal
       v-slot="{ cancel, ok }"
       v-model="doShowBook_FormModal"
@@ -93,11 +67,8 @@
 import { ref , onMounted } from 'vue'
 import { useLocalStorage } from '@vueuse/core'
 import { useBooks } from './composables/useBooks'
-import BookCards from './widgets/BookCards.vue'
-import BookTable from './widgets/BooksTable.vue'
 import ApiBookTable from './widgets/ApiBooksTable.vue'
 import ApiBookCards from './widgets/ApiBookCards.vue'
-import EditBookForm from './widgets/EditBookForm.vue'
 import ApiEditBookForm from './widgets/ApiEditBookForm.vue'
 import { Book } from './types'
 import { useModal, useToast } from 'vuestic-ui'
@@ -107,14 +78,8 @@ const doShowAsCards = useLocalStorage('books-view', true)
 
 const { books, update, add, isLoading, remove, pagination, sorting } = useBooks()
 
-const bookToEdit = ref<Book | null>(null)
 const doShowBookFormModal = ref(false)
 const doShowBook_FormModal = ref(false)
-
-const editBook = (book: Book) => {
-  bookToEdit.value = book
-  doShowBookFormModal.value = true
-}
 
 const book_ToEdit = ref<Book | null>(null)
 
@@ -128,23 +93,6 @@ const createNewBook = () => {
 }
 
 const { init: notify } = useToast()
-
-const onBookSaved = async (book: Book) => {
-  doShowBookFormModal.value = false
-  if ('id' in book) {
-    await update(book as Book)
-    notify({
-      message: 'Book updated',
-      color: 'success',
-    })
-  } else {
-    await add(book as Book)
-    notify({
-      message: 'Book created',
-      color: 'success',
-    })
-  }
-}
 
 const on_BookSaved = async(book : Book)=>{
   doShowBookFormModal.value = false
@@ -182,26 +130,6 @@ const on_BookSaved = async(book : Book)=>{
 
 const { confirm } = useModal()
 
-const onBookDeleted = async (book: Book) => {
-  const response = await confirm({
-    title: 'Delete book',
-    message: `Are you sure you want to delete book "${book.book_name}"?`,
-    okText: 'Delete',
-    size: 'small',
-    maxWidth: '380px',
-  })
-
-  if (!response) {
-    return
-  }
-
-  await remove(book)
-  notify({
-    message: 'Book deleted',
-    color: 'success',
-  })
-}
-
 const on_BookDeleted = async (book: Book) => {
   const response = await confirm({
     title: 'Delete book',
@@ -223,23 +151,6 @@ const on_BookDeleted = async (book: Book) => {
 
   // After deleteing, call getAllBooks to refresh the data
   await getAllBooks();
-}
-
-const editFormRef = ref()
-
-const beforeEditFormModalClose = async (hide: () => unknown) => {
-  if (editFormRef.value.isFormHasUnsavedChanges) {
-    const agreed = await confirm({
-      maxWidth: '380px',
-      message: 'Form has unsaved changes. Are you sure you want to close it?',
-      size: 'small',
-    })
-    if (agreed) {
-      hide()
-    }
-  } else {
-    hide()
-  }
 }
 
 const edit_FormRef = ref()
