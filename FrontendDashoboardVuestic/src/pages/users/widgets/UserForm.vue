bcae7f6885158db6942fa8d21898d28e75e6ccb5
Shenel Kurera <wsakurera@gmail.com>
2024-11-08 20:12:47 +0100

    Create UserForm modal

---
+++ FrontendDashoboardVuestic/src/pages/users/widgets/UserForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>edit</h1>
+</template>
+
+<script>
+export default {
+  name: 'UserForm',
+}
+</script>
--- FrontendDashoboardVuestic/src/pages/users/widgets/UsersTable.vue
+++ FrontendDashoboardVuestic/src/pages/users/widgets/UsersTable.vue
@@ -1,60 +1,51 @@
 <template>
-   <div class="flex flex-col md:flex-row gap-2 mb-2 justify-between">
-        <div class="flex flex-col md:flex-row gap-2 justify-start">        
-        </div>
-        <VaButton @click="addUser">Add User</VaButton>
+  <div class="flex flex-col md:flex-row gap-2 mb-2 justify-between">
+    <div class="flex flex-col md:flex-row gap-2 justify-start">
     </div>
+    <VaButton @click="addUser">Add User</VaButton>
+  </div>
 
-    <VaDataTable
-      :items="paginatedUsers"
-      :columns="columns"
-      :loading="loading"
-      v-model:sort-by="sortBy"
-      v-model:sorting-order="sortingOrder"
-      hoverable
+  <VaDataTable :items="paginatedUsers" :columns="columns" :loading="loading" v-model:sort-by="sortBy"
+    v-model:sorting-order="sortingOrder" hoverable>
+    <template #cell(created_at)="{ rowData }">
+      <div class="flex items-center gap-2 ellipsis max-w-[230px]">
+        {{ formatDate(rowData.created_at) }}
+      </div>
+    </template>
+  </VaDataTable>
+
+  <div class="flex flex-col-reverse md:flex-row gap-2 justify-between items-center py-2">
+    <!-- Results per page dropdown -->
+    <div>
+      <b>{{ totalUsers }} results.</b> <!-- Show total results -->
+      Results per page
+      <VaSelect v-model="itemsPerPage" class="!w-20" :options="[10, 20, 50, 100]" />
+    </div>
+    <!-- Pagination buttons and page number display -->
+    <div v-if="totalPages > 1" class="flex items-center gap-2">
+      <VaButton preset="secondary" icon="va-arrow-left" aria-label="Previous page" :disabled="currentPage === 1"
+        @click="currentPage--" />
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <VaButton preset="secondary" icon="va-arrow-right" aria-label="Next page" :disabled="currentPage === totalPages"
+        @click="currentPage++" />
+      <VaPagination v-model="currentPage" :pages="totalPages" :visible-pages="5" :boundary-links="false"
+        :direction-links="false" />
+    </div>
+  </div>
+
+  <VaModal
+      v-slot="{ cancel, ok }"
+      v-model="doShowUserModal"
+      size="small"
+
     >
-      <template #cell(created_at)="{ rowData }">
-        <div class="flex items-center gap-2 ellipsis max-w-[230px]"> 
-          {{ formatDate(rowData.created_at) }}
-        </div>
-      </template>
-    </VaDataTable>
-
-    <div class="flex flex-col-reverse md:flex-row gap-2 justify-between items-center py-2">
-      <!-- Results per page dropdown -->
-      <div>
-        <b>{{ totalUsers }} results.</b> <!-- Show total results -->
-        Results per page
-        <VaSelect v-model="itemsPerPage" class="!w-20" :options="[10, 20, 50, 100]" />
-      </div>
-      <!-- Pagination buttons and page number display -->
-      <div v-if="totalPages > 1" class="flex items-center gap-2">
-        <VaButton
-          preset="secondary"
-          icon="va-arrow-left"
-          aria-label="Previous page"
-          :disabled="currentPage === 1"
-          @click="currentPage--"
-        />
-        <span>Page {{ currentPage }} of {{ totalPages }}</span>
-        <VaButton
-          preset="secondary"
-          icon="va-arrow-right"
-          aria-label="Next page"
-          :disabled="currentPage === totalPages"
-          @click="currentPage++"
-        />
-        <VaPagination
-          v-model="currentPage"
-          :pages="totalPages"
-          :visible-pages="5"
-          :boundary-links="false"
-          :direction-links="false"
-        />
-      </div>
-    </div>
-    
-  </template>
+    yolo
+      <!-- <h1 v-if="projectToEdit === null" class="va-h5 mb-4">Add project</h1>
+      <h1 v-else class="va-h5 mb-4">Edit project</h1>-->
+      <UserForm ref="userFormRef"/>
+     
+    </VaModal>
+</template>
 
 <script>
 import { ref, computed, watch } from "vue";
@@ -62,26 +53,30 @@ import axios from "axios";
 import { defineVaDataTableColumns } from 'vuestic-ui'
 import moment from "moment";
 import { addUser } from "../../../data/pages/users";
+import UserForm from './UserForm.vue'
 
 export default {
+  components: {
+    UserForm,
+  },
   data() {
     return {
-        allUsers : [],    // Initialize as an empty array
-        columns : defineVaDataTableColumns([
-          { label: 'User name', key: 'name', sortable: true },
-          { label: 'Email', key: 'email', sortable: true },
-          { label: 'Roles', key: 'roles', sortable: true },
-          { label: 'Created on', key: 'created_at', sortable: true },
-          { label: ' ', key: 'actions' },
-        ]),
-        loading: true,  // Optional: indicate loading state
-        // Sorting and pagination states
-        sortBy: "name", // Default sorting column
-        sortingOrder: "asc", // Default sorting order
-        currentPage: 1, // Current page number
-        itemsPerPage: 10, // Items per page
-        doShowUserModal: false,
-      }
+      allUsers: [],    // Initialize as an empty array
+      columns: defineVaDataTableColumns([
+        { label: 'User name', key: 'name', sortable: true },
+        { label: 'Email', key: 'email', sortable: true },
+        { label: 'Roles', key: 'roles', sortable: true },
+        { label: 'Created on', key: 'created_at', sortable: true },
+        { label: ' ', key: 'actions' },
+      ]),
+      loading: true,  // Optional: indicate loading state
+      // Sorting and pagination states
+      sortBy: "name", // Default sorting column
+      sortingOrder: "asc", // Default sorting order
+      currentPage: 1, // Current page number
+      itemsPerPage: 10, // Items per page
+      doShowUserModal: false,
+    }
   },
   computed: {
     // Total items count
@@ -116,29 +111,30 @@ export default {
       return Math.ceil(this.totalUsers / this.itemsPerPage);
     }
   },
-  mounted: function() {
+  mounted: function () {
     this.getAllUsers();
   },
   methods: {
-    getAllUsers(){
-        this.loading = true;  // Set loading to true while fetching
-        axios.get('api/users')
-            .then(response => {
-                this.allUsers = response.data;
-                console.log("all users : "+ JSON.stringify(response.data))
-            })
-            .catch(err =>{
-                console.log("errs :"+ err)
-            })
-            .finally(() => {
-              this.loading = false;  // Reset loading to false once finished
-            });
+    getAllUsers() {
+      this.loading = true;  // Set loading to true while fetching
+      axios.get('api/users')
+        .then(response => {
+          this.allUsers = response.data;
+          console.log("all users : " + JSON.stringify(response.data))
+        })
+        .catch(err => {
+          console.log("errs :" + err)
+        })
+        .finally(() => {
+          this.loading = false;  // Reset loading to false once finished
+        });
     },
     formatDate(date) {
       return moment(date).format('DD-MMM-YYYY HH:mm');
     },
-    addUser(){
-      alert("adduserAlert")
+    addUser() {
+      this.doShowUserModal = true;
+
     },
   },
 
